3a97b0cea9344c71e61fbca9c0c57b7e54ea350b
Tyler Stanish <tystanish@gmail.com>
2020-02-12 13:17:54 -0500

    test example for MenuBar

---
+++ client-react/src/menubar/MenuBar.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react';
+
+import MenuBar from './MenuBar';
+
+
+test('Shows the title', () => {
+  const {getByText} = render(<MenuBar/>)
+  expect(getByText('Fortune')).toBeInTheDocument()
+})
+
+test('Shows dropdown item only after clicking on dropdown', () => {
+  const {getByText, queryByText} = render(<MenuBar/>)
+  // get* methods will throw an exception if nothing matches,
+  // query* methods will return null instead
+  expect(queryByText('Action')).toBeNull()
+  fireEvent.click(getByText('Dropdown'))
+  expect(getByText('Action')).toBeInTheDocument()
+})
