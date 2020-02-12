469994d8a1d273122d828f808349959c02700067
Blake Steel <blakemsteel@gmail.com>
2020-02-12 12:36:46 -0500

    add menubar

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MenuBar from './menubar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <MenuBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ client-react/src/menubar/MenuBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+export default class MenuBar extends Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Navbar.Brand href="home">
+                <img
+                    alt=""
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                Fortune
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown" alignRight>
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
