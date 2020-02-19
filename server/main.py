f454f88e1064ce4739749c4fd0b0c3088fcf0482
Tyler Stanish <tystanish@gmail.com>
2020-02-19 16:46:42 -0500

    finishing merge

--- server/main.py
+++ server/main.py
@@ -6,15 +6,18 @@ from flask_cors import CORS
 
 from auth.routes import auth_bp
 from errors.handlers import errors_bp
-from db import db
+from db import * # FIXME get rid of * when you have db migrations
 
 def create_app():
+    db.create_tables([Profile, AuthToken, Game, GameProfile, Coin, GameCoin,
+        Ticker, Trade, GameProfileCoin])
+    # FIXME get rid of this when you have db migrations
     app = Flask(__name__)
     CORS(app)
 
     @app.before_request
     def before_request():
-        db.connect()
+        db.connect(reuse_if_open=True)
         #pass
 
     @app.after_request
