c5b769a9b4f8e0c213b838b1f723236a6f91b360
Sam Kravitz <samkravitz99@gmail.com>
2020-02-23 18:35:16 -0500

    Add edit game route

--- server/game/routes.py
+++ server/game/routes.py
@@ -20,9 +20,25 @@ def create():
             ends_at = ends_at
         )
     except Exception as e:
-        return "Failure to create Game: " + str(e)
+        return "Failure to create Game: {}".format(str(e))
 
     db.session.add(game)
     db.session.commit()
     return "Game created. Game id={}".format(game.id)
+
+@game_bp.route('/edit/<id>', methods=['POST'])
+    def edit():
+    # edit game
+    try:
+        game = Game.query.filter_by(id=id).first()
+        game.starting_cash = request.args.get('starting_cash')
+        game.shareable_link = request.args.get('sharable_link')
+        game.shareable_code = request.args.get('sharable_code')
+        game.ends_at = request.args.get('ends_at')
+    except Exception as e:
+        return "Failure to edit Game: {}".format(str(e))
+    
+    db.session.add(game)
+    db.session.commit()
+    return "Game edited. Game id={}".format(game.id)
 
