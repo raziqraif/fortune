e2b1c470c86c5bd39ac0a8460548318a7a3bcbbe
Tyler Stanish <tystanish@gmail.com>
2020-02-28 10:52:43 -0500

    added more seed coins

--- server/main.py
+++ server/main.py
@@ -16,6 +16,9 @@ def create_app():
         Coin.create(name='Bitcoin', symbol='BTC')
         Coin.create(name='Ethereum', symbol='ETH')
         Coin.create(name='Litecoin', symbol='LTC')
+        Coin.create(name='Coin 3', symbol='CO3')
+        Coin.create(name='Coin 4', symbol='CO4')
+        Coin.create(name='Coin 5', symbol='CO5')
     except:
         pass
     # FIXME get rid of this when you have db migrations
