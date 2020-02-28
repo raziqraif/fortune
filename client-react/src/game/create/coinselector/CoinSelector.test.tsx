e93c036c7c0c995178c7217bb68e95c2a6f365d6
Raziq Raif <42981908+raziqraif@users.noreply.github.com>
2020-02-27 19:49:50 -0500

    Add initial coin selector tests

--- client-react/src/game/create/coinselector/CoinSelector.test.tsx
+++ client-react/src/game/create/coinselector/CoinSelector.test.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {render, fireEvent, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import CoinSelector from './CoinSelector';
+import {getAllCoins} from "../../../redux/actions/Coins";
 
 const allCoins = [
     {
         id: '1',
-        name: 'coin'
+        name: 'coin1'
+    },
+    {
+        id: '2',
+        name: 'coin2'
     }
-]
+];
+
 const activeCoins: Array<{ id: string, name: string }> = [];
 const setActiveCoins = (activeCoins: Array<{ id: string, name: string }>) => {};
 
@@ -20,4 +27,68 @@ test('Renders CoinSelector filter label', () => {
             setActiveCoins={setActiveCoins}/>
     );
     expect(getByText('Coins available to your players')).toBeInTheDocument()
-})
+});
+
+test('Renders all coins', () => {
+    const {getByText} = render(
+        <CoinSelector
+            allCoins={allCoins}
+            activeCoins={activeCoins}
+            setActiveCoins={setActiveCoins}/>
+    );
+    allCoins.forEach((coin) => {
+        expect(getByText(coin.name)).toBeInTheDocument()
+    });
+});
+
+test('Select/Deselect all button functionality',() => {
+    const {getByText} = render(
+        <CoinSelector
+            allCoins={allCoins}
+            activeCoins={activeCoins}
+            setActiveCoins={setActiveCoins}/>
+    );
+    expect(getByText('Select All')).toBeInTheDocument()
+    expect(getByText('Deselect All')).toBeInTheDocument()
+});
+
+test('Filter an invalid coin', ()=>{
+    const {queryByText, getByPlaceholderText} = render(
+        <CoinSelector
+            allCoins={allCoins}
+            activeCoins={activeCoins}
+            setActiveCoins={setActiveCoins}/>
+    );
+    let txtFilter = getByPlaceholderText('Filter coins')
+    expect(txtFilter).toBeInTheDocument()
+    userEvent.type(txtFilter, 'INVALID_COIN_X')
+    expect(txtFilter).toHaveAttribute('value', 'INVALID_COIN_X')
+    allCoins.forEach((coin) => {
+        expect(queryByText(coin.name)).toBeNull()
+    });
+});
+
+test('Filter all valid coin', ()=>{
+    const {getByText, queryByText, getByPlaceholderText} = render(
+        <CoinSelector
+            allCoins={allCoins}
+            activeCoins={activeCoins}
+            setActiveCoins={setActiveCoins}/>
+    );
+    let txtFilter = getByPlaceholderText('Filter coins')
+    let index = 0
+    userEvent.type(txtFilter, allCoins[index].name)
+    expect(txtFilter).toHaveAttribute('value', allCoins[index].name)
+    allCoins.forEach((coin) => {
+        if (coin.name != allCoins[index].name) {
+            expect(queryByText(coin.name)).toBeNull()
+        }
+        else {
+            expect(queryByText(coin.name)).toBeInTheDocument()
+        }
+    });
+
+    expect(getByText('coin1')).toBeInTheDocument()
+});
+
+
