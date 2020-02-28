7cf5872378842e85045632ecd039d895052273cf
Tyler Stanish <tystanish@gmail.com>
2020-02-28 09:49:49 -0500

    fixed breaking changes in Login and Register tests when using redux

--- client-react/src/login/Login.test.tsx
+++ client-react/src/login/Login.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render, fireEvent, screen} from '@testing-library/react';
 
+import {deeplyRenderComponentWithRedux} from '../test-utils';
 import Login from './Login';
 
 
 test('Should render login page', () => {
-  const {getByText} = render(<Login/>)
+  const {getByText} = deeplyRenderComponentWithRedux(<Login/>)
   expect(getByText('Login')).toBeInTheDocument()
 })
--- client-react/src/register/Register.test.tsx
+++ client-react/src/register/Register.test.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {render, fireEvent, screen} from '@testing-library/react';
 
 import Register from './Register';
+import {deeplyRenderComponentWithRedux} from '../test-utils'
 
 
 test('Should render register page', () => {
-  const {getByText} = render(<Register/>)
+  const {getByText} = deeplyRenderComponentWithRedux(<Register/>)
   expect(getByText('Register')).toBeInTheDocument()
 })
