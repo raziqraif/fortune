a516ba31a1c7be88b456eb906e926d345420dbbc
Sam Kravitz <samkravitz99@gmail.com>
2020-03-06 19:52:26 -0500

    Add all columns to Cointable

--- client-react/src/game/Game.tsx
+++ client-react/src/game/Game.tsx
@@ -6,117 +6,118 @@ import { connect } from 'react-redux';
 import { GameType } from '../redux/actions/Game'
 import CSS from 'csstype';
 import moment from 'moment';
+import Cointable from './Cointable';
 
 export interface GameProps {
-    getGame: (
-        id: number
-    ) => void;
-    gameId?: string;
-    error: string;
-    game: GameType;
+	getGame: (
+		id: number
+	) => void;
+	gameId?: string;
+	error: string;
+	game: GameType;
 }
 
 interface GameState {
-    days: string;
-    hours: string;
-    minutes: string;
-    seconds: string;
+	days: string;
+	hours: string;
+	minutes: string;
+	seconds: string;
 }
 
 const styles: { [name: string]: CSS.Properties } = {
-    heading: {
-        width: '100%',
-        borderBottom: 'medium solid'
-    },
+	heading: {
+		width: '100%',
+		borderBottom: 'medium solid'
+	},
 };
 
 class Game extends React.Component<GameProps, GameState> {
-    private interval!: NodeJS.Timeout;
+	private interval!: NodeJS.Timeout;
 
-    constructor(props: GameProps) {
-        super(props);
+	constructor(props: GameProps) {
+		super(props);
 
-        this.state = {
-            days: '',
-            hours: '',
-            minutes: '',
-            seconds: '',
-        }
-    }
+		this.state = {
+			days: '',
+			hours: '',
+			minutes: '',
+			seconds: '',
+		}
+	}
 
-    componentDidMount() {
-        const { gameId } = this.props;
-        // private game - get game
-        // LOOKATME - getGame and the backend expect gameId to be a number,
-        // but since gameId comes from match.params, it is a string.
-        // not sure how we should unify this discrepency
-        if (gameId) {
-            this.props.getGame(parseInt(gameId));
-        }
-        
-        // TODO global game - get global game 
-        else {
+	componentDidMount() {
+		const { gameId } = this.props;
+		// private game - get game
+		// LOOKATME - getGame and the backend expect gameId to be a number,
+		// but since gameId comes from match.params, it is a string.
+		// not sure how we should unify this discrepency
+		if (gameId) {
+			this.props.getGame(parseInt(gameId));
+		}
 
-        }
+		// TODO global game - get global game 
+		else {
 
-        // moment countdown interval
-        this.interval = setInterval(this.countdown, 1000);
+		}
 
-    }
+		// moment countdown interval
+		this.interval = setInterval(this.countdown, 1000);
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+	}
 
-    // calculates how much time is left in the game
-    private countdown = () => {
-        const endsAt = moment(this.props.game.data.endsAt);
-        const now = moment();
-        const diff = moment(endsAt.diff(now));
-        const days = diff.format('D');
-        const hours = diff.format('HH');
-        const minutes = diff.format('mm');
-        const seconds = diff.format('ss');
-        this.setState({ days, hours, minutes, seconds });
-    }
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
 
-    render() {
-        const { gameId, error, game } = this.props;
-        const { days, hours, minutes, seconds } = this.state;
-        if (error) {
-            return <p style={{color: 'red'}}>{error}</p>
-        }
+	// calculates how much time is left in the game
+	private countdown = () => {
+		const endsAt = moment(this.props.game.data.endsAt);
+		const now = moment();
+		const diff = moment(endsAt.diff(now));
+		const days = diff.format('D');
+		const hours = diff.format('HH');
+		const minutes = diff.format('mm');
+		const seconds = diff.format('ss');
+		this.setState({ days, hours, minutes, seconds });
+	}
 
-        return (
-            <div className="Game">
-                <Container>
-                    <Row>
-                        <h1 style={styles.heading}>Fortune</h1>
-                    </Row>
-                    <Row>
-                        <h3 style={styles.heading}>{gameId ? `Private Game: ${game.data.name}` : `Global Game`}</h3>
-                        <span>Ends in: {days} {hours} {minutes} {seconds} </span>
-                        
-                    </Row>
-                    <Row>
-                        Game info will go here
-                    </Row>
-                    <Row>
-                        Table will go here
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+	render() {
+		const { gameId, error, game } = this.props;
+		const { days, hours, minutes, seconds } = this.state;
+		if (error) {
+			return <p style={{ color: 'red' }}>{error}</p>
+		}
+
+		return (
+			<div className="Game">
+				<Container>
+					<Row>
+						<h1 style={styles.heading}>Fortune</h1>
+					</Row>
+					<Row>
+						<h3 style={styles.heading}>{gameId ? `Private Game: ${game.data.name}` : `Global Game`}</h3>
+						<span>Ends in: {days} {hours} {minutes} {seconds} </span>
+
+					</Row>
+					<Row>
+						Game info will go here
+          </Row>
+					<Row>
+						<Cointable />
+					</Row>
+				</Container>
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = (state: RootState) => ({
-    game: state.game.game,
-    error: state.game.setGameErrorMessage,
+	game: state.game.game,
+	error: state.game.setGameErrorMessage,
 })
 
 const mapDispatchToProps = {
-    getGame: Actions.game.getGame,
+	getGame: Actions.game.getGame,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
