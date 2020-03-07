dd0875dbd707a33311ee7bb8f9b0a7c9f4405c38
Sam Kravitz <samkravitz99@gmail.com>
2020-03-06 22:42:58 -0500

    Add row in table for each coin

--- client-react/src/game/CoinTable/CointableCoin.tsx
+++ client-react/src/game/CoinTable/CointableCoin.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Row, Col } from 'react-bootstrap';
 
 interface CointableCoinProps {
 	coin: { id: string, name: string };
@@ -6,16 +7,39 @@ interface CointableCoinProps {
 
 class CointableCoin extends React.Component<CointableCoinProps> {
 
-
-    
-    render() {
-        const { name } = this.props.coin;
-        return (
-            <div className="Coin">
-                {name}
-            </div>
-        )
-    }
+	render() {
+		const { name } = this.props.coin;
+		return (
+			<div className="Coin">
+				<Row>
+					<Col>
+						<span>{name}</span>
+					</Col>
+					<Col>
+						<span>$100</span>
+					</Col>
+					<Col>
+						<span>^^_^_^^^___^^</span>
+					</Col>
+					<Col>
+						<span>+.4%</span>
+					</Col>
+					<Col>
+						<span>_______</span>
+					</Col>
+					<Col>
+						<span>Buy</span>
+					</Col>
+					<Col>
+						<span>Sell</span>
+					</Col>
+					<Col>
+						<span>1.05</span>
+					</Col>
+				</Row>
+			</div>
+		)
+	}
 }
 
 export default CointableCoin
