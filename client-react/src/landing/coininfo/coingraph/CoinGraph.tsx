edcda1d7a9a97bf9046b0eb7a8473aca2b235320
Ryan Huff <mr.ryanhuff@live.com>
2020-03-10 22:11:58 -0400

    create coingraph files

---
+++ client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Table } from 'react-bootstrap';
+import Actions from '../../redux/actions';
+import { RootState } from '../../redux/reducers';
+import { connect } from 'react-redux';
+
+export default class CoinInfo extends React.Component {
+
+    render() {
+        return ()
+    }//render()
+}//class CoinGraph
