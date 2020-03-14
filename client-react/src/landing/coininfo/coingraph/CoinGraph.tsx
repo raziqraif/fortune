0d2f59494037155220b92c0dd6ba3df181ff64cc
Ryan Huff <mr.ryanhuff@live.com>
2020-03-14 15:23:39 -0400

    intial charting with apex

--- client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
+++ client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
+import Chart from 'react-apexcharts'
 
 export default class CoinGraph extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          toolbar: {
+            show: false
+          },
+          id: "history",
+        },//chart
+      },//options
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    };//this.state
+  }//constructor
 
     render() {
         return (
-          <div>test</div>
+          <div>
+            <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="line"
+            height="100"
+            width="500"/>
+          </div>
         )
     }//render()
 }//class CoinGraph
