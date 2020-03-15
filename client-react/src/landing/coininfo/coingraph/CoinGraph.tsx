0bbac4f7e6906a84333b2ae286c54c6b54091862
Ryan Huff <mr.ryanhuff@live.com>
2020-03-14 20:54:33 -0400

    Change from ApexCharts to react-sparklines. A whole lot better.

--- client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
+++ client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
@@ -1,63 +1,17 @@
 import * as React from 'react';
-import Chart from 'react-apexcharts'
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 export default class CoinGraph extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      options: {
-        chart: {
-          zoom: {
-            enabled: false
-          },
-          toolbar: {
-            show: false
-          },
-          id: "history",
-        },//chart
-
-        tooltip: {
-          enabled: false
-        },
-        xaxis: {
-          axisBorder: {
-            show: false,
-          },
-          labels:{
-            show:false,
-          },//labels
-        },//xaxis
-        yaxis: {
-          labels:{
-            show: false
-          },//labels
-          min: function(min) { return min}, // Most realistically, the price min will never equal 0
-                                            // therefore, need a function to adjust min
-        //  max: 100,
-        },//yaxis
-        grid: {
-          show:false
-        }
-      },//options
-      series: [
-        {
-          data: [10, 40, 12, 0, 49, 0, 70, 91]
-        }
-      ]
-    };//this.state
-  }//constructor
+  }
 
     render() {
         return (
-          <div>
-            <Chart
-            options={this.state.options}
-            series={this.state.series}
-            type="line"
-            height="50%"
-            width="50%"/>
-          </div>
+          <Sparklines data={[5, 12, 30, 0, 50, 5, 20]}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
         )
     }//render()
 }//class CoinGraph
