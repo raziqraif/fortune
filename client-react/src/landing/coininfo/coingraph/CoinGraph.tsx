e951a469b78b982a8326af70099447291ef3e3c5
Ryan Huff <mr.ryanhuff@live.com>
2020-03-14 21:00:55 -0400

    move data to state

--- client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
+++ client-react/src/landing/coininfo/coingraph/CoinGraph.tsx
@@ -4,12 +4,15 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default class CoinGraph extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [5, 12, 30, 0, 50, 5, 20]
+    }
 
   }
 
     render() {
         return (
-          <Sparklines data={[5, 12, 30, 0, 50, 5, 20]}>
+          <Sparklines data={this.state.data}>
             <SparklinesLine color="blue" />
           </Sparklines>
         )
