09baa86c001df74ae7faca3d1a594df6c147d9e9
Ryan Huff <mr.ryanhuff@live.com>
2020-03-22 15:50:23 -0400

    very crude websocket implmentation - only shows first object for every coin, only shows price, etc.

--- client-react/src/landing/coininfo/CoinInfo.tsx
+++ client-react/src/landing/coininfo/CoinInfo.tsx
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import Actions from '../../redux/actions';
 import { RootState } from '../../redux/reducers';
 import { connect } from 'react-redux';
+import io from 'socket.io-client'
 
 import CoinGraph from './coingraph/CoinGraph'
 
@@ -14,17 +15,33 @@ interface CoinInfoProps {
 class CoinInfo extends React.Component<CoinInfoProps> {
   constructor(props: CoinInfoProps) {
       super(props);
+      this.state ={
+        data:{},
+      }
   }
 
   componentDidMount() {
       this.props.getAllCoins();
+      const socket = io('http://localhost:5000').connect();
+      socket.on('message', function(data:any){
+        this.setState({data:data});
+        console.log('yuh', this.state.data);
+      }.bind(this));
+  }
+
+  private showPrice(num:number) {
+    if(this.state.data[num]){
+      return this.state.data[num].price
+    } else {
+      return <div>Retrieving...</div>
+    }
   }
 
 private dynamicRowRender() {
   let rows = [];
   rows = this.props.allCoins.map(coin => <tr>
                                          <td>{coin.name} ({coin.symbol})</td>
-                                         <td>Price</td>
+                                         <td>Price: {this.showPrice(0)}</td>
                                          <td><div align="center"><CoinGraph/></div></td>
                                          <td>Change</td>
                                          </tr> );
