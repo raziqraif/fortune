f6a774bd56159d734c7ce128109a95d31bda052c
Sam Kravitz <samkravitz99@gmail.com>
2020-03-24 09:11:45 -0400

    Add transaction confirmation modal

--- client-react/src/game/CoinTable/CointableCoin.tsx
+++ client-react/src/game/CoinTable/CointableCoin.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Button, InputGroup, FormControl, Modal, Row } from 'react-bootstrap';
+import CSS from 'csstype';
 
 interface CointableCoinProps {
 	coin: { id: string, name: string };
@@ -8,6 +9,9 @@ interface CointableCoinProps {
 
 interface CointableCoinState {
 	amount: string;
+	showConfirm: boolean;
+	errMes: string;
+	confirmMes: string;
 }
 
 enum transactionType {
@@ -15,12 +19,23 @@ enum transactionType {
 	SELL
 }
 
+const styles: { [name: string]: CSS.Properties } = {
+	errMes: {
+		justifyContent: 'center',
+		color: 'red',
+	},
+
+};
+
 class CointableCoin extends React.Component<CointableCoinProps, CointableCoinState> {
 
 	constructor(props: CointableCoinProps) {
 		super(props);
 		this.state = {
-			amount: ''
+			amount: '',
+			showConfirm: false,
+			errMes: '',
+			confirmMes: '',
 		}
 	}
 
@@ -30,17 +45,39 @@ class CointableCoin extends React.Component<CointableCoinProps, CointableCoinSta
 
 	private transaction = (type: transactionType) => {
 		const amount = Number(this.state.amount);
-		if (!amount || amount <= 0) return;
-		console.log(amount);
+		this.toggleConfirm();
+		if (!amount || amount <= 0) { // invalid input
+			this.setState({ errMes: 'Please enter a valid amount.' });
+		} else {
+			// build dialogue
+			let confirmMes = "Are you sure you want to ";
+			confirmMes += type === transactionType.BUY ? "buy" : "sell";
+			confirmMes += " " + amount;
+			confirmMes += " " + this.props.coin.name + "?";
+			this.setState({ confirmMes });
+		}
+	}
+
+	private toggleConfirm = () => {
+		const { showConfirm } = this.state;
+		this.setState({ showConfirm: !showConfirm });
+
+		// closing modal, so clear messages
+		if (!showConfirm) {
+			this.setState({ errMes: '' });
+			this.setState({ confirmMes: '' });
+		}
 	}
 
 	render() {
 		const { name } = this.props.coin;
 		const { price } = this.props;
+		const { showConfirm, errMes, confirmMes } = this.state;
+		const price_f = price.substring(0, price.indexOf('.') + 3); // 2 digits after decimal
 		return (
 			<tr>
 				<td>{name}</td>
-				<td>{price}</td>
+				<td>${price_f}</td>
 				<td>^^_^_^^^____^_^_^_</td>
 				<td>+.4%</td>
 				<td>
@@ -57,6 +94,28 @@ class CointableCoin extends React.Component<CointableCoinProps, CointableCoinSta
 				<td><Button variant="outline-primary" onClick={() => this.transaction(transactionType.BUY)}>Buy</Button></td>
 				<td><Button variant="outline-danger" onClick={() => this.transaction(transactionType.SELL)}>Sell</Button></td>
 				<td>1.05</td>
+
+				{/* confirmation modal */}
+				<Modal show={showConfirm} onHide={this.toggleConfirm}>
+					<Modal.Header closeButton>
+						<Modal.Title>Confirm Transaction</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Row style={{ justifyContent: 'center' }}>{confirmMes}</Row>
+						{
+							errMes &&
+							<Row style={styles.errMes}>{errMes}</Row>
+						}
+					</Modal.Body>
+					<Modal.Footer style={{ justifyContent: 'center' }}>
+						{/* Only show confirm button if there are no errors */}
+						{
+							!errMes &&
+							<Button variant="primary" onClick={this.toggleConfirm}>Confirm</Button>
+						}
+						<Button variant="secondary" onClick={this.toggleConfirm}>Cancel</Button>
+					</Modal.Footer>
+				</Modal>
 			</tr>
 		)
 	}
