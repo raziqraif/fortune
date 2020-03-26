6162154e0603fd7b79dd350ee184e509f0179087
Tyler Stanish <tystanish@gmail.com>
2020-03-26 14:42:43 -0500

    added more unit and integration tests for real-time data service

--- server/tests/integration/test_service.py
+++ server/tests/integration/test_service.py
@@ -9,7 +9,6 @@ from tests.utils import DbTest
 
 class TestService(DbTest):
     @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
-        # NOTE these must be actual coins defined in the migrations!
         {'symbol': 'BTC', 'price': '56.12345678', '1d': {'price_change_pct': '-2.33334567'}},
     ])))
     def test_parsing_with_one_coin(self):
@@ -21,7 +20,32 @@ class TestService(DbTest):
         self.assertEqual(btc_ticker.price_change_day_pct, Decimal('-2.33334567'))
 
     @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
-        # NOTE these must be actual coins defined in the migrations!
+        {'symbol': 'BTC', 'price': '56.12345678', '1d': {'price_change_pct': '-2.33334567'}},
+    ])))
+    def test_parsing_with_many_ping_calls(self):
+        os.environ['NOMICS_BASE_URL'] = 'foo'
+        ping('BTC')
+        btc = Coin.get(Coin.symbol == 'BTC')
+        btc_ticker = Ticker.get(Ticker.coin == btc)
+        self.assertEqual(btc_ticker.price, Decimal('56.12345678'))
+        self.assertEqual(btc_ticker.price_change_day_pct, Decimal('-2.33334567'))
+        Ticker.delete().execute()
+        ping('BTC')
+        btc = Coin.get(Coin.symbol == 'BTC')
+        btc_ticker = Ticker.get(Ticker.coin == btc)
+        self.assertEqual(btc_ticker.price, Decimal('56.12345678'))
+        self.assertEqual(btc_ticker.price_change_day_pct, Decimal('-2.33334567'))
+
+    @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
+    ])))
+    def test_parsing_with_no_coins(self):
+        os.environ['NOMICS_BASE_URL'] = 'foo'
+        num_tickers_before = Ticker.select().count()
+        ping()
+        num_tickers_after = Ticker.select().count()
+        self.assertEqual(num_tickers_before, num_tickers_after)
+
+    @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
         {'symbol': 'BTC', 'price': '56.12345678', '1d': {'price_change_pct': '-1.23456789'}},
         {'symbol': 'ETH', 'price': '42.98765432', '1d': {'price_change_pct': '-8.98765432'}},
     ])))
@@ -36,4 +60,23 @@ class TestService(DbTest):
         self.assertEqual(btc_ticker.price_change_day_pct, Decimal('-1.23456789'))
         self.assertEqual(eth_ticker.price, Decimal('42.98765432'))
         self.assertEqual(eth_ticker.price_change_day_pct, Decimal('-8.98765432'))
+
+    @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
+        {'symbol': 'DASH', 'price': '56.12345678', '1d': {'price_change_pct': '-1.23456789'}},
+    ])))
+    def test_parsing_with_four_character_symbol(self):
+        os.environ['NOMICS_BASE_URL'] = 'foo'
+        ping('DASH')
+        dash = Coin.get(Coin.symbol == 'DASH')
+        ticker = Ticker.get(Ticker.coin == dash)
+        self.assertEqual(ticker.price, Decimal('56.12345678'))
+        self.assertEqual(ticker.price_change_day_pct, Decimal('-1.23456789'))
+
+    @patch('requests.get', MagicMock(return_value=Mock(json=lambda: [
+        {'symbol': 'FOO', 'price': '56.12345678', '1d': {'price_change_pct': '-1.23456789'}},
+    ])))
+    def test_raises_error_if_token_not_exists(self):
+        os.environ['NOMICS_BASE_URL'] = 'foo'
+        with self.assertRaises(Exception):
+            ping('FOO')
 
