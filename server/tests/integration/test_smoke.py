aae067afb8cb7d64e1ce79ddbfa5e2231f9327ab
Tyler Stanish <tystanish@gmail.com>
2020-03-27 16:16:08 -0500

    added more smoke tests

--- server/tests/integration/test_smoke.py
+++ server/tests/integration/test_smoke.py
@@ -1,3 +1,5 @@
+import json
+
 from main import SocketIO
 
 from tests.utils import DbTest, AuthDbTest
@@ -21,3 +23,25 @@ class SmokeTest(AuthDbTest):
     def test_websocket_connection(self):
         sc = self.socketio.test_client(self.app, flask_test_client=self.client)
         self.assertTrue(sc.is_connected())
+
+    def test_can_use_server_while_using_websocket(self):
+        sc = self.socketio.test_client(self.app, flask_test_client=self.client)
+        self.assertTrue(sc.is_connected())
+        res = self.client.get('/')
+        self.assertEqual('hello world', res.data.decode())
+
+    def test_robust_can_use_server_while_using_websocket(self):
+        sc = self.socketio.test_client(self.app, flask_test_client=self.client)
+        self.assertTrue(sc.is_connected())
+        res = self.client.post(
+            '/auth/register',
+            data=json.dumps({'username': 'theusername', 'password': 'password'}),
+            content_type='application/json'
+        )
+        self.assertEqual(200, res._status_code)
+        res = self.client.post(
+            '/auth/login',
+            data=json.dumps({'username': 'theusername', 'password': 'password'}),
+            content_type='application/json'
+        )
+        self.assertEqual(200, res._status_code)
