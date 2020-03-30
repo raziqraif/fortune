91a46f342dbf1c6682d69eb20de551b93140b66a
Ryan Huff <mr.ryanhuff@live.com>
2020-03-30 19:41:12 -0400

    remove lorem ipsum frmo jumbotron

--- client-react/src/landing/Landing.tsx
+++ client-react/src/landing/Landing.tsx
@@ -8,8 +8,8 @@ export default class Landing extends React.Component {
         return (
             <div>
             <Jumbotron>
-              <h1> Welcome to Fortune! </h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum enim facilisis gravida neque convallis a. Fermentum leo vel orci porta non pulvinar. Eget duis at tellus at urna condimentum mattis. Euismod quis viverra nibh cras pulvinar mattis nunc sed. Pretium viverra suspendisse potenti nullam ac tortor vitae purus. Convallis aenean et tortor at risus viverra. Facilisis gravida neque convallis a cras semper auctor. Neque aliquam vestibulum morbi blandit cursus risus. Quisque non tellus orci ac auctor augue. Consectetur a erat nam at. Facilisis magna etiam tempor orci eu. Sit amet mattis vulputate enim nulla aliquet. Integer feugiat scelerisque varius morbi. Nec sagittis aliquam malesuada bibendum arcu vitae.</p>
+              <h1> Fortune: A Cryptocurrency Trading Game </h1>
+
             </Jumbotron>
 
             <CoinInfo/>
