eba6aac16a4b6d57c0a5942431d463f03e586792
Ryan Huff <mr.ryanhuff@live.com>
2020-04-15 14:55:36 -0400

    Display error message if no coins are in the db

--- client-react/src/landing/coininfo/CoinInfo.tsx
+++ client-react/src/landing/coininfo/CoinInfo.tsx
@@ -102,6 +102,11 @@ private dynamicRowRender() {
                                            </tr> );
     rows = rows.slice(0,10) //only show first 10 coins - in reality need to filter through rows for certain coins
   }
+  if(rows.length === 0){
+    rows = <tr key={0}>
+            No coins available to display
+           </tr>
+  }
   return rows
 }
 
