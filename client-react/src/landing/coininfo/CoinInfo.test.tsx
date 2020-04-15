fbb132dfd31e1b662dcf025caa7420fce359e968
Ryan Huff <mr.ryanhuff@live.com>
2020-04-15 00:24:45 -0400

    begin landing page tests

--- client-react/src/landing/coininfo/CoinInfo.test.tsx
+++ client-react/src/landing/coininfo/CoinInfo.test.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 import {render, fireEvent, screen} from '@testing-library/react';
 
 import CoinInfo from './CoinInfo';
+import { deeplyRenderComponentWithRedux } from '../../test-utils';
+
+const emptyProps = {
+  allCoins: {
+    coin: {
+      id: '',
+      name: '',
+      symbol: '',
+      number: '',
+    },
+    prices: {
+      price: '',
+      captured_at: new Date(),
+      price_change_day_pct: '',
+      id: '',
+    },
+  }
+}
+
+const sampleData = {
+  ...emptyProps,
+  coin: {
+    ...emptyProps.coin
+  },
+  prices: {
+    ...emptyProps.prices,
+    price: '11'
+  }
+}
 
 
-test('Should render Landing page', () => {
-  const {getByText} = render(<Landing/>)
-  expect(getByText('Landing')).toBeInTheDocument() //TODO
+test('Should display correct coin data', () => {
+  const {getByText} = deeplyRenderComponentWithRedux(<CoinInfo
+          allCoins={sampleData}
+          />);
+  expect(getByText('11')).toBeInTheDocument() //TODO
 })
