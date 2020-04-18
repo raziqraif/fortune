fc2bf3b9c07000e893217be69d8344f5499d3ff4
Blake Steel <blakemsteel@gmail.com>
2020-04-18 16:10:24 -0400

    Add tests

--- server/tests/integration/test_profile.py
+++ server/tests/integration/test_profile.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 import json
+import bcrypt
 
 from db import *
 from tests.utils import AuthDbTest
@@ -10,18 +11,19 @@ class ProfileTests(AuthDbTest):
         with db.atomic() as txn:
             self.profile = Profile.create(
                 username='username',
-                hashed_password='password'
+                hashed_password=bcrypt.hashpw('password'.encode(), bcrypt.gensalt()).decode()
             )
             self.token = AuthToken.create(profile=self.profile, token='validtoken').token
+            self.headers = {
+                'Authorization': 'Bearer ' + self.token
+            }
 
     def test_change_username_with_existing_username(self):
         res = self.client.put('/auth/username',
             data=json.dumps({
                 'username': 'username'
             }),
-            headers={
-                'Authorization': 'Bearer ' + self.token,
-            },
+            headers=self.headers,
             content_type='application/json',
         )
         self.assertEqual(int(HTTPStatus.BAD_REQUEST), res._status_code)
@@ -32,12 +34,36 @@ class ProfileTests(AuthDbTest):
             data=json.dumps({
                 'username': new_username
             }),
-            headers={
-                'Authorization': 'Bearer ' + self.token,
-            },
+            headers=self.headers,
             content_type='application/json',
         )
         self.assertEqual(int(HTTPStatus.OK), res._status_code)
         existing_profile = Profile.get_or_none(Profile.id == self.profile.id)
         assert existing_profile is not None
         self.assertEqual(existing_profile.username, new_username)
+
+    def test_change_password_with_incorrect_old_password(self):
+        res = self.client.put('/auth/password',
+            data=json.dumps({
+                'oldPassword': 'not_password',
+                'newPassword': 'newPassword',
+            }),
+            headers=self.headers,
+            content_type='application/json',
+        )
+        self.assertEqual(int(HTTPStatus.BAD_REQUEST), res._status_code)
+
+    def test_change_password_success(self):
+        new_password = 'new_password'
+        res = self.client.put('/auth/password',
+            data=json.dumps({
+                'oldPassword': 'password',
+                'newPassword': new_password
+            }),
+            headers=self.headers,
+            content_type='application/json',
+        )
+        self.assertEqual(int(HTTPStatus.OK), res._status_code)
+        existing_profile = Profile.get_or_none(Profile.id == self.profile.id)
+        assert existing_profile is not None
+        assert bcrypt.checkpw(new_password.encode(), existing_profile.hashed_password.encode())
