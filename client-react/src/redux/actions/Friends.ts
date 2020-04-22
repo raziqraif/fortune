8b5e6b07302789f14509ea674d13ce056b1874f5
Ryan Huff <mr.ryanhuff@live.com>
2020-04-22 12:08:04 -0400

    change post to put for accept method - should be operational

--- client-react/src/redux/actions/Friends.ts
+++ client-react/src/redux/actions/Friends.ts
@@ -6,9 +6,6 @@ import {push} from 'connected-react-router'
 import {handleAxiosError} from './Utils'
 import { fetchAuthToken } from './Auth'
 
-
-
-
 export const sendFriendRequest = (requester, requestee, status) => {
   return async (dispatch: Dispatch<Action>) => {
     try {
@@ -19,12 +16,13 @@ export const sendFriendRequest = (requester, requestee, status) => {
       handleAxiosError(e, dispatch, Type.FRIEND_FAILED);
     }
   }
+}
 
 export const acceptFriendRequest = (requester, requestee, status) => {
   return async (dispatch: Dispatch<Action>) => {
     try {
       await fetchAuthToken();
-      const res = await axios.post('http://localhost:5000/friends/accept', {requester, requestee, status});
+      const res = await axios.put('http://localhost:5000/friends/accept', {requester, requestee, status});
       dispatch({type: Type.ACCEPT_FRIEND_REQUEST, payload: res })
     } catch (e) {
       handleAxiosError(e, dispatch, Type.FRIEND_FAILED);
