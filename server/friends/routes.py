a63c7f2cf92ed8263b71a38a93df3cf5d77ea956
Ryan Huff <mr.ryanhuff@live.com>
2020-04-22 01:15:44 -0400

    implement accepting requests on backend

--- server/friends/routes.py
+++ server/friends/routes.py
@@ -3,7 +3,7 @@ from werkzeug.exceptions import BadRequest
 
 from auth.decorators import require_authentication
 from db import Profile, Friends
-from .services import create_request, get_friends_by_username
+from .services import create_request, get_friends_by_username, accept_request
 from .serializers import FriendsSerializer
 
 
@@ -20,6 +20,24 @@ def send(profile):
     if friend_req is None:
         raise BadRequest('Could not create request')
     return jsonify(FriendsSerializer.serialize(friend_req))
+
+@friends_bp.route('/accept', methods=['PUT'])
+@require_authentication
+def accept(profile):
+    validated_data: dict = FriendsSerializer.deserialize(request.json)
+    requester_name = validated_data['requester']
+    requestee_name = validated_data['requestee']
+    status = validated_data['status']
+    accept = accept_request(requester_name, requestee_name)
+    if accept is None:
+        raise BadRequest('Could not accept request')
+
+    try:
+        q = Friends.update({Friends.status: accept.status}).where(Friends.id == accept.id)
+        q.execute()
+    except Exception as e:
+        return "Failed to accept request: {}".format(str(e))
+    return jsonify(FriendsSerializer.serialize(accept))
 
 @friends_bp.route('/<username>', methods=['GET'])
 def get_by_username(username):
--- server/friends/services.py
+++ server/friends/services.py
@@ -5,14 +5,14 @@ from notifications.services import send_notification
 
 @db.atomic()
 def create_request(requester_name, requestee_name, status):
-    requester_profile = Profile.select().where(Profile.username == requester_name).get()
-    requestee_profile = Profile.select().where(Profile.username == requestee_name).get()
+    requester_profile = Profile.get_or_none(Profile.username == requester_name)
+    requestee_profile = Profile.get_or_none(Profile.username == requestee_name)
     if requester_profile is None:
         raise BadRequest('Could not find requester')
     if requestee_profile is None:
         raise BadRequest('Could not find requestee')
-    check_duplicates = Friends.select().where((Friends.requester == requester_profile and Friends.requestee == requestee_profile)
-                                    or (Friends.requester == requestee_profile and Friends.requestee == requester_profile)).get()
+    check_duplicates = Friends.get_or_none((Friends.requester == requester_profile and Friends.requestee == requestee_profile)
+                                    or (Friends.requester == requestee_profile and Friends.requestee == requester_profile))
     if check_duplicates is not None:
         raise BadRequest('Friend request has already been made')
     req = Friends.create(
@@ -22,6 +22,20 @@ def create_request(requester_name, requestee_name, status):
     send_notification(requestee_profile, f'{requester_profile.username} has sent you a friend request!')
     return req
 
+@db.atomic()
+def accept_request(requester_name, requestee_name):
+    requester_profile = Profile.get_or_none(Profile.username == requester_name)
+    requestee_profile = Profile.get_or_none(Profile.username == requestee_name)
+    if requester_profile is None:
+        raise BadRequest('Could not find requester')
+    if requestee_profile is None:
+        raise BadRequest('Could not find requestee')
+    req = Friends.get_or_none((Friends.requester == requester_profile and Friends.requestee == requestee_profile)
+                                    or (Friends.requester == requestee_profile and Friends.requestee == requester_profile))
+    req.status = 1
+    return req
+
+@db.atomic()
 def get_friends_by_username(username):
     get_friends = Friends.select().where((Friends.requester == username or Friends.requestee == username) and Friends.status == 1)
     if get_friends.count() == 0:
